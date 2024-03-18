56a5f694cd1c7a0652667a1907590aeda2dc4a32
MOHANRAJ KANDASAMY <con_mkandasamy@focusbrands.com>
2024-03-18 11:02:12 -0700

    Code optimized

--- src/useApplePay.tsx
+++ src/useApplePay.tsx
@@ -13,44 +13,53 @@ export const useApplePay = (priceToPay: string): [() => void, any] => {
   const getSupportedNetworks =
     process.env.REACT_APP_APPLE_SUPPORTED_NETWORKS?.split("|");
 
+  const paymentRequest = () => {
+    return {
+      countryCode: process.env.REACT_APP_APPLE_COUNTRY_CODE,
+      currencyCode: process.env.REACT_APP_APPLE_CURRENCY_CODE,
+      merchantCapabilities: getMerchantCapabilities,
+      supportedNetworks: getSupportedNetworks,
+      total: {
+        label: process.env.REACT_APP_APPLE_PAY_LABEL,
+        type: "final",
+        amount: priceToPay,
+      },
+    };
+  };
+
+  const validateMerchant = (merchantSession: any) => {
+    return async (event: any) => {
+      const response = (
+        await axios.post("/api/wallet/apple-pay/get-merchant-session", {
+          validationURL: event.validationURL,
+        })
+      ).data;
+      if (response.data) {
+        merchantSession.completeMerchantValidation(response.data);
+      } else {
+        merchantSession.abort();
+      }
+    };
+  };
+
+  const paymentAuthorized = (merchantSession: any) => {
+    return (event: any) => {
+      const paymentStatus = {
+        status: appleSession.STATUS_SUCCESS,
+      };
+      if (paymentStatus.status === 0) {
+        merchantSession.completePayment(paymentStatus);
+        setAppleResponse(event.payment);
+      } else {
+        merchantSession.abort();
+      }
+    };
+  };
   const handleOnApplePay = useCallback(async () => {
-    console.log("---", process.env.REACT_APP_APPLE_COUNTRY_CODE);
     if (canMakeApplePayments && priceToPay) {
-      const paymentRequest = {
-        countryCode: process.env.REACT_APP_APPLE_COUNTRY_CODE,
-        currencyCode: process.env.REACT_APP_APPLE_CURRENCY_CODE,
-        merchantCapabilities: getMerchantCapabilities,
-        supportedNetworks: getSupportedNetworks,
-        total: {
-          label: process.env.REACT_APP_APPLE_PAY_LABEL,
-          type: "final",
-          amount: priceToPay,
-        },
-      };
-      const merchantSession = new appleSession(3, paymentRequest);
-      merchantSession.onvalidatemerchant = async (event: any) => {
-        const response = (
-          await axios.post("/api/wallet/apple-pay/get-merchant-session", {
-            validationURL: event.validationURL,
-          })
-        ).data;
-        if (response.data) {
-          merchantSession.completeMerchantValidation(response.data);
-        } else {
-          merchantSession.abort();
-        }
-      };
-      merchantSession.onpaymentauthorized = (event: any) => {
-        const paymentStatus = {
-          status: appleSession.STATUS_SUCCESS,
-        };
-        if (paymentStatus.status === 0) {
-          merchantSession.completePayment(paymentStatus);
-          setAppleResponse(event.payment);
-        } else {
-          merchantSession.abort();
-        }
-      };
+      const merchantSession = new appleSession(3, paymentRequest());
+      merchantSession.onvalidatemerchant = validateMerchant(merchantSession);
+      merchantSession.onpaymentauthorized = paymentAuthorized(merchantSession);
       merchantSession.oncancel = () => {
         merchantSession.completePayment(appleSession.STATUS_FAILURE);
       };
